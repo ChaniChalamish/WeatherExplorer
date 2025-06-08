acf4440cb9188053fc0fe6bf333e9cb593442bf2
ChaniChalamish <b0527626958@gmail.com>
2025-06-09 00:47:29 +0300

    style the css

--- src/components/NoResultsDiv.jsx
+++ src/components/NoResultsDiv.jsx
@@ -1,7 +1,7 @@
 const NoResultsDiv = () => {
   return (
-    <div className="no-results">
-      <img src="icons/no-result.svg" alt="No results found" className="icon" />
+    <div className="noResult">
+      <img src="icons/no-result.svg" alt="No results found" className="Icon" />
       <h3 className="title">Something went wrong!</h3>
       <p className="message">We&apos;re unable to retrieve the weather details. Enure you&apos;ve entered a valid city or try again later.</p>
     </div>
